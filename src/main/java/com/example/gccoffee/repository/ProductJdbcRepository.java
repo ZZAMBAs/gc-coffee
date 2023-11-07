8e5bd1b16942f7f1462eeaa73c574b1d3c70df93
ZZAMBAs <rksidksrksi@naver.com>
2023-11-08 00:58:20 +0900

    Feat: Product CRUD를 구현한 Repository를 추가한다.

---
+++ src/main/java/com/example/gccoffee/repository/ProductJdbcRepository.java
@@ -0,0 +1,107 @@
+package com.example.gccoffee.repository;
+
+import com.example.gccoffee.model.Category;
+import com.example.gccoffee.model.Product;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+import static com.example.gccoffee.JdbcUtils.toLocalDateTime;
+import static com.example.gccoffee.JdbcUtils.toUUID;
+
+@Repository
+public class ProductJdbcRepository implements ProductRepository {
+    private final NamedParameterJdbcTemplate jdbcTemplate;
+
+    public ProductJdbcRepository(NamedParameterJdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public List<Product> findAll() {
+        return jdbcTemplate.query("SELECT * FROM products", productRowMapper);
+    }
+
+    @Override
+    public Product insert(Product product) {
+        var updateCnt = jdbcTemplate.update("INSERT INTO products VALUES (UUID_TO_BIN(:productId), :productName, :category, :price, :description, :createdAt, :updatedAt)",
+                toParamMap(product));
+        if (updateCnt != 1) {
+            throw new RuntimeException("업데이트 된 값이 없거나 여러 건입니다.");
+        }
+        return product;
+    }
+
+    @Override
+    public Product update(Product product) {
+        int updateCnt = jdbcTemplate.update("UPDATE products SET product_name = :productName, " +
+                "category = :category, price = :price, description = :description, updated_at = :updatedAt " +
+                "WHERE product_id = UUID_TO_BIN(:productId)", toParamMap(product));
+
+        if (updateCnt != 1) {
+            throw new RuntimeException("업데이트 된 값이 없습니다.");
+        }
+
+        return product;
+    }
+
+    @Override
+    public Optional<Product> findById(UUID productId) {
+        try {
+            return Optional.ofNullable(jdbcTemplate.queryForObject("SELECT * FROM products WHERE product_id = UUID_TO_BIN(:productId)",
+                    Collections.singletonMap("productId", productId.toString().getBytes()), productRowMapper));
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public Optional<Product> findByName(String productName) {
+        try {
+            return Optional.ofNullable(jdbcTemplate.queryForObject("SELECT * FROM products WHERE product_name = :productName",
+                    Collections.singletonMap("productName", productName), productRowMapper));
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public List<Product> findByCategory(Category category) {
+        return jdbcTemplate.query("SELECT * FROM products WHERE category = :category",
+                Collections.singletonMap("category", category.toString()), productRowMapper);
+    }
+
+    @Override
+    public void deleteAll() {
+        jdbcTemplate.update("DELETE FROM products", Collections.emptyMap());
+    }
+
+    private static final RowMapper<Product> productRowMapper = (rs, rowNum) -> {
+        UUID productId = toUUID(rs.getBytes("product_id"));
+        String productName = rs.getString("product_name");
+        var category = Category.valueOf(rs.getString("category"));
+        var price = rs.getLong("price");
+        var description = rs.getString("description");
+        var createdAt = toLocalDateTime(rs.getTimestamp("created_at"));
+        var updatedAt = toLocalDateTime(rs.getTimestamp("updated_at"));
+
+        return new Product(productId, productName, category, price, description, createdAt, updatedAt);
+    };
+
+    private static Map<String, Object> toParamMap(Product product){
+        var paramMap = new HashMap<String, Object>();
+        paramMap.put("productId", product.getProductId().toString().getBytes());
+        paramMap.put("productName", product.getProductName());
+        paramMap.put("category", product.getCategory().toString());
+        paramMap.put("price", product.getPrice());
+        paramMap.put("description", product.getDescription());
+        paramMap.put("createdAt", product.getCreatedAt());
+        paramMap.put("updatedAt", product.getUpdatedAt());
+
+        return paramMap;
+    }
+}
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS products (
+    product_id BINARY(16) PRIMARY KEY,
+    product_name VARCHAR(20) NOT NULL,
+    category VARCHAR(50) NOT NULL,
+    price BIGINT NOT NULL,
+    description VARCHAR(500) DEFAULT NULL,
+    created_at DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+    updated_at DATETIME(6) DEFAULT NULL
+);
