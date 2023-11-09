5ca911bcfb535ee0bef19c104b21b4a1cf71740e
ZZAMBAs <rksidksrksi@naver.com>
2023-11-09 10:39:04 +0900

    Feat: Product 생성 로직과 페이지를 구현한다.

--- src/main/java/com/example/gccoffee/controller/ProductController.java
+++ src/main/java/com/example/gccoffee/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.example.gccoffee.service.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -28,5 +29,14 @@ public class ProductController {
     @GetMapping("/new-product")
     public String newProductPage() {
         return "new-product";
+    }
+
+    @PostMapping("/products")
+    public String newProduct(CreateProductRequest createProductRequest) {
+        productService.createProduct(createProductRequest.productName(),
+                createProductRequest.category(),
+                createProductRequest.price(),
+                createProductRequest.description());
+        return "redirect:/products";
     }
 }
