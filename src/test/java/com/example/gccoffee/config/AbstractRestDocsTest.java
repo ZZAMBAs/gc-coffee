bebcf35395bbca81a357744b417716afc9e9a3e2
ZZAMBAs <rksidksrksi@naver.com>
2023-11-12 21:36:34 +0900

    Feat: Rest Docs 관련 구성 클래스를 생성한다..

---
+++ src/test/java/com/example/gccoffee/config/AbstractRestDocsTest.java
@@ -0,0 +1,35 @@
+package com.example.gccoffee.config;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Import;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+
+@Import(RestDocsConfiguration.class)
+@ExtendWith(RestDocumentationExtension.class)
+public abstract class AbstractRestDocsTest { // API 명세를 만드는 테스트들은 이것을 상속
+    @Autowired
+    protected RestDocumentationResultHandler restDocs;
+
+    protected MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp(final WebApplicationContext context, final RestDocumentationContextProvider restDocumentation) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
+                .apply(documentationConfiguration(restDocumentation))
+                .alwaysDo(MockMvcResultHandlers.print())
+                .alwaysDo(restDocs)
+                .addFilters(new CharacterEncodingFilter("UTF-8", true))
+                .build();
+    }
+}
